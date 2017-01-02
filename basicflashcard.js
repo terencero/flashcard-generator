cedcdadbc82c476f004fb47a893d1c080edd7d38
Terence Ro <terencero@Terences-MacBook-Air.local>
2017-01-02 17:38:46 -0600

    add recursion to basic flash card prompt

---
+++ basicflashcard.js
@@ -0,0 +1,75 @@
+var inquirer = require('inquirer');
+
+var fs = require('fs');
+
+var cardResults = [{}];
+// var cardResults = [];
+
+var constructAFlashCard = function(){
+
+function BasicFlashCard(category, question, answer){
+	this.category = category;
+	this.question = question;
+	this.answer = answer;
+
+}
+
+BasicFlashCard.prototype.printCard = function(){
+		console.log('Category: ' + this.category + '\nQuestion: ' + this.question + '\nAnswer: ' + this.answer);
+};
+
+BasicFlashCard.prototype.askAQuestion = function(){
+		console.log('Question: ' + this.question);
+};
+
+BasicFlashCard.prototype.revealAnswer = function(){
+		console.log('Answer: ' + this.answer);
+};
+var count = 0;
+var createObject = function(){
+	
+	if(count < 3){
+		inquirer.prompt([
+		{	
+			name: 'category',
+			message: 'Please enter a category for your flash card.'
+		},
+
+		{
+			name: 'userQuestion',
+			message: 'Please enter the question for your flash card.'
+		},
+
+		{
+			name: 'userAnswer',
+			message: 'Please enter the answer for your flash card.'
+		}
+
+	]).then(function(response){
+		var newBasicFlashCard = new BasicFlashCard(response.category, response.userQuestion, response.userAnswer);
+
+		cardResults.push(newBasicFlashCard);
+		
+		console.log(cardResults);
+
+		count++;
+		createObject();
+	});
+	} else {
+		console.log('Flash cards made.');
+	}
+};	
+createObject();
+};
+
+
+// var basicFlashCardObj = {
+	
+// 	objectconstructor: BasicFlashCard(),
+// 	create: createObject()
+
+// };
+
+module.exports = constructAFlashCard();
+module.exports = cardResults;
+
---
+++ flashcards.js
@@ -0,0 +1,27 @@
+var basicflashcard = require('./basicflashcard.js');
+
+var clozecard = require('./clozecard.js');
+
+var inquirer = require('inquirer');
+
+var fs = require('fs');
+
+
+// fs.readFile('log.txt', 'utf8', function(err, data){
+// 	output = data;
+// 	console.log(output);
+// });
+
+
+
+// console.log(basicflashcard);
+
+
+// ---------------------node front end code--------------------------------------------
+// var createBasicFlashCard = function() {
+
+
+// };
+// createBasicFlashCard();
+
+
